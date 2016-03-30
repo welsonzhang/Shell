ba07877c363edbeebd4d9c09d48a58e421e9dbf4
welsonzhang <942469560@qq.com>
2016-03-30 21:46:07 +0800

    update

---
+++ ps/pcpu_usage.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#count the process cpu 
+
+SECS=3600
+UNIT_TIME=60
+
+STEP=$(( $SECS / $UNIT_TIME ))
+
+echo Watching CPU usage... ;
+
+for((i=0;i<STEPS;i++))
+do
+    ps -eocomm,pcpu |tail -n +2 >> /tmp/cpu_usage.$$
+    sleep $UNIT_TIME
+done
+
+echo
+echo CPU eaters:
+
+cat /tmp/cpu_usage.$$ | \
+awk '
+{ process[$1]+=$2;}
+END{
+    for(i in process)
+    {
+        printf("%-20s %s\n",i,process[i]);
+    }
+    }' |sort -nrk 2|head
+
+rm /tmp/cpu_usage.$$
+
